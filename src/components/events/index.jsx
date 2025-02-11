65777a6678db1cff7a92139a7608d7adc7cfeb5c
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-02-11 03:38:21 -0500

    treat featured

--- src/components/events/index.jsx
+++ src/components/events/index.jsx
@@ -10,16 +10,18 @@ export default function Events() {
       <h1>Events</h1>
       {sortedEvents.map((data, index) => {
         const title = `🥃 Round ${sortedEvents.length - index}: ${data.theme}`;
+        const featured = new Date(data.date) > new Date();
         return (
           <div
             key={data.id}
-            className="events-event"
+            className={`events-event ${featured ? 'featured' : ''}`}
           >
             <div className="events-event-content">
               <h3>{title}</h3>
               <p>{data.date}</p>
               <p>{data.description}</p>
               <a href={data.location.map} target="map">{data.location.name}</a>
+              {featured && <button type="button">Sign Up!</button>}
               <img src={data.photo} />
             </div>
           </div>
