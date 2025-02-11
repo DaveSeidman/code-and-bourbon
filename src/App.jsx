530ac3930cba5b60a4c664fcafdf21081596f9a0
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-02-11 00:51:29 -0500

    hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useParams, useNavigate } from 'react-router-dom';
 import headerImg from './assets/images/header.png';
+import Hero from './components/hero';
 import logo from './assets/images/logo.svg';
 import './index.scss';
 
@@ -14,30 +15,27 @@ export default function App() {
           path="/:page?"
           element={(
             <div className="app">
-              <div className="header">
+              {/* <div className="header">
                 <div className="header-image">
                   <img src={headerImg} />
                 </div>
                 <img className="header-logo" src={logo} />
-              </div>
+              </div> */}
+              <Hero />
+
               <div className="pages">
                 <div className="pages-page current">
-                  <h1>What's Up?</h1>
+                  <h1>JoinUs</h1>
                   <p>The next meetup is scheduled for February 27th 2025</p>
                   <p>Click here to register</p>
                 </div>
                 <div className="pages-page who">
-                  <h1>Who We Are</h1>
+                  <h1>WhoWeAre</h1>
                   <p>We are a group of like-minded individuals, mostly in NYC, that are reclaiming the post-work happy hour we lost during the pandemic. Most of us enjoy Bourbon but it's not at all a requirement, but more of a suggestion that our meetups ecourage a relaxed environment where natural creativity and networking opportinities.</p>
                 </div>
                 <div className="pages-page where">
-                  <h1>Where to Find Us</h1>
+                  <h1>WhereWeAre</h1>
                   <p>We meet in various bars and coworking spaces in Brooklyn and Manhattan on a monthly basis.</p>
-                  <p>Join the Discord server here to stay up to date.</p>
-
-                </div>
-                <div className="pages-page history">
-                  <h1>History</h1>
                   <ul>
                     <li>Jan 2024: Windsor Terrace</li>
                     <li>Feb 2024: Dumbo</li>
@@ -46,9 +44,10 @@ export default function App() {
                     <li>Jun 2024:  Windsor Terrace</li>
                     <li>Sep 2024: Park Slope</li>
                   </ul>
+                  <p>Join the Discord server here to stay up to date.</p>
                 </div>
                 <div className="pages-page join">
-                  <h1>One of Us! One of Us!</h1>
+                  <h1>OneOfUsOneOfUs</h1>
                   <p>Join us by setting up a profile here: link</p>
                   <p>You'll receive information about how to join us in person at the next metup!d</p>
                 </div>
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,12 +1,86 @@
 // TODO, create a color, ASCII art version of the header image, then react to pointer events by swapping portions of the image out with the ascii for that location, let the ascii art hang for a few seconds and fade back to the image so that the user can create trails of code by dragging that still resemble the image
 
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import headerImg from '../../assets/images/header.png';
+import logo from '../../assets/images/logo.svg';
+
+import './index.scss';
 
 export default function Hero() {
-  const [colums, setColums] = 
+  const rem = 16;
+  const size = 60;
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [arrayLength, setArrayLength] = useState(0);
+  const [cells, setCells] = useState(new Array(100).fill({ active: false }));
+
+  const cellsRef = useRef();
+
+  useEffect(() => {
+    const resize = () => {
+      setWidth(cellsRef.current.getBoundingClientRect().width);
+      setHeight(cellsRef.current.getBoundingClientRect().height);
+    };
+    addEventListener('resize', resize);
+
+    resize();
+    return (() => {
+      removeEventListener('resize', resize);
+    });
+  }, []);
+
+  useEffect(() => {
+    // console.log(width, height);
+    const cols = Math.ceil(width / size);
+    const rows = Math.ceil(height / size);
+    setArrayLength(cols * rows);
+  }, [width, height]);
+
+  useEffect(() => {
+    setCells(new Array(arrayLength).fill(null).map((_, index) => {
+      const cols = Math.ceil(width / size);
+      const rows = Math.ceil(height / size);
+      console.log('left', index, cols, index % cols);
+      return ({
+        id: `cell-${index}`,
+        active: false,
+        top: Math.floor(index / cols) * size,
+        left: (index % cols) * size,
+      });
+    }));
+  }, [arrayLength]);
+
   return (
     <div className="hero">
-
+      <div className="hero-image">
+        <img src={headerImg} />
+      </div>
+      <div
+        className="hero-cells"
+        ref={cellsRef}
+      >
+        {
+          cells.map((cell) => (
+            <span
+              key={cell.id}
+              className={`hero-cells-cell ${cell.active ? 'active' : ''}`}
+              style={{
+                top: cell.top,
+                left: cell.left,
+                width: size,
+                height: size,
+              }}
+              onPointerMove={(e) => {
+                e.target.classList.add('active');
+                setTimeout(() => { e.target.classList.remove('active'); }, 2000);
+              }}
+            />
+          ))
+        }
+      </div>
+      <div className="hero-logo">
+        <img className="hero-logo-image" src={logo} />
+      </div>
     </div>
   );
 }
