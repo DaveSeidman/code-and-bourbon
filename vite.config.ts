72df70e5e938661c1e994482f54edb74f92a0e69
Ivo Ilić <3476570+ivoilic@users.noreply.github.com>
2026-02-12 02:52:27 -0500

    Setup for render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
@@ -12,6 +13,7 @@ export default defineConfig({
     tanstackStart({
       srcDirectory: 'src',
     }),
+    nitro(),
     // react's vite plugin must come after start's vite plugin
     viteReact(),
   ],
